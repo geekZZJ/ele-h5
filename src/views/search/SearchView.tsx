729ee6097771779f10437fd5d59f7f13aa35b076
zzj <2461927976@qq.com>
2024-01-31 23:28:14 +0800

    feat: search组件

--- src/views/search/SearchView.tsx
+++ src/views/search/SearchView.tsx
@@ -1,9 +1,12 @@
-import { TransitionGroup, defineComponent, ref } from 'vue'
+import { TransitionGroup, computed, defineComponent, ref, watch } from 'vue'
 import './SearchView.less'
 import OpSearch from '@/components/OpSearch/OpSearch'
 import { fetchSearchData } from '@/api/search'
 import type { ISearchResult } from '@/types'
 import { Icon } from 'vant'
+import { useToggle } from '@/use/useToggle'
+
+const history_tags = ['披萨', '栗子', '炒饭', '玉米', '烧烤', '水果', '牛奶', '奶茶']
 
 export default defineComponent({
   name: 'SearchView',
@@ -11,9 +14,15 @@ export default defineComponent({
   setup(props, { emit }) {
     const searchValue = ref('')
     const searchResult = ref<ISearchResult[]>([])
+    const searchState = ref(-1)
+    const historyTags = computed(() =>
+      isHistoryTagShown.value ? history_tags : history_tags.slice(0, 5)
+    )
+    const [isHistoryTagShown, toggleHistoryTag] = useToggle(false)
 
     const onSearch = async (v: string) => {
       console.log('search', v)
+      searchState.value = 0
       // const { list } = await fetchSearchData(v)
       const list = [
         {
@@ -27,8 +36,22 @@ export default defineComponent({
           resultCount: 1
         }
       ]
+      searchState.value = 1
       searchResult.value = list
     }
+
+    const onTagClick = (v: string) => {
+      searchValue.value = v
+      onSearch(v)
+    }
+
+    watch(searchValue, (newVal: string) => {
+      if (!newVal) {
+        searchResult.value = []
+        return
+      }
+      onSearch(newVal)
+    })
 
     return () => (
       <div class="search-view">
@@ -40,30 +63,41 @@ export default defineComponent({
           onSearch={onSearch}
           onCancel={() => emit('cancel')}
         ></OpSearch>
-        {/* <div v-if="!searchValue" class="search-view__history">
-          <div class="label">历史搜索</div>
-          <TransitionGroup name="list">
-            <div class="history-tag" v-for="v in historyTags" key="v" onClick={() => onTagClick(v)}>
-              {{ v }}
-            </div>
-            <div class="history-tag" key="arrow" onClick={toggleHistoryTag}>
-              <VanIcon v-if="isHistoryTagShown" name="arrow-up"></VanIcon>
-              <VanIcon v-else name="arrow-down"></VanIcon>
-            </div>
-          </TransitionGroup>
-        </div> */}
-
-        <div class="search-view__result">
-          {/* <div class="searching" v-if="searchState === DOING">~正在搜索~</div> */}
-          {searchResult.value.map((v) => (
-            <div class="result-item" key={v.label}>
-              <Icon name="search"></Icon>
-              <div class="name">{v.label}</div>
-              <div class="count">约{v.resultCount}个结果</div>
-            </div>
-          ))}
-          {!searchResult.value.length && <div class="no-result">~暂无推荐~</div>}
-        </div>
+        {searchValue.value ? (
+          <div class="search-view__result">
+            {searchState.value === 0 && <div class="searching">~正在搜索~</div>}
+            {searchState.value === 1 && (
+              <div>
+                {searchResult.value.map((v) => (
+                  <div class="result-item" key={v.label}>
+                    <Icon name="search"></Icon>
+                    <div class="name">{v.label}</div>
+                    <div class="count">约{v.resultCount}个结果</div>
+                  </div>
+                ))}
+                {!searchResult.value.length && <div class="no-result">~暂无推荐~</div>}
+              </div>
+            )}
+          </div>
+        ) : (
+          <div class="search-view__history">
+            <div class="label">历史搜索</div>
+            <TransitionGroup name="list">
+              {historyTags.value.map((v) => (
+                <div class="history-tag" key={v} onClick={() => onTagClick(v)}>
+                  {v}
+                </div>
+              ))}
+              <div class="history-tag" key="arrow" onClick={toggleHistoryTag}>
+                {isHistoryTagShown.value ? (
+                  <Icon name="arrow-up"></Icon>
+                ) : (
+                  <Icon name="arrow-down"></Icon>
+                )}
+              </div>
+            </TransitionGroup>
+          </div>
+        )}
       </div>
     )
   }
