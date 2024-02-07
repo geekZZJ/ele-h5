36f9caccd3da8d309359427e26aac6377ba4c49d
zzj <2461927976@qq.com>
2024-02-07 12:28:20 +0800

    feat: useAsync hooks

--- src/types/home.d.ts
+++ src/types/home.d.ts
@@ -12,3 +12,25 @@ export interface ISearchResult {
   label: string
   resultCount: number
 }
+
+export interface IHomeInfo {
+  banner: IBanner
+  searchRecommends: ISearchRecommend[]
+  transformer: ITransformer[]
+  countdown: ICountdown
+  activities: string[]
+}
+
+interface IBanner {
+  imgUrl: string
+}
+
+interface ITransformer {
+  imgUrl: string
+  label: string
+}
+
+interface ICountdown {
+  time: number
+  goods: IGood
+}
--- src/views/home/HomeView.tsx
+++ src/views/home/HomeView.tsx
@@ -3,6 +3,9 @@ import './HomeView.less'
 import TheTop from './components/TheTop'
 import { useToggle } from '@/use/useToggle'
 import SearchView from '../search/SearchView'
+import { useAsync } from '@/use/useAsync'
+import { fetchHomePageData } from '@/api/home'
+import type { IHomeInfo } from '@/types'
 
 export default defineComponent({
   name: 'HomeView',
@@ -18,12 +21,15 @@ export default defineComponent({
       }
     ]
     const [isSearchViewShow, toggleSearchView] = useToggle(false)
+    const { data, pending } = useAsync(fetchHomePageData, {} as IHomeInfo, true)
     return () => (
       <div class="home-page">
         <Transition name="fade">
           {isSearchViewShow.value && <SearchView onCancel={toggleSearchView}></SearchView>}
         </Transition>
         <TheTop recommends={recommends} onSearchClick={toggleSearchView} />
+        {pending}
+        {data}
       </div>
     )
   }
